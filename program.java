cadbaeabb7fb6543e0a674e58a5981b194e60c94
unknown <inf132237@lab-43-6.cs.put.poznan.pl>
2018-10-31 10:33:13 +0100

    Dodano komentarz nr 1

--- program.java
+++ program.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+				//Komentarz nr 1
 		            System.out.println("Hello, World");
 			    System.out.println("New feature!");
 	}
